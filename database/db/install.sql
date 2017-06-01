0a896cf724439dfa6edd0f8a7e31fc65e867a240
Andrey Zhidenkov <pensnarik@gmail.com>
2017-06-01 18:56:19 +0300

    Добавил VIEW

--- database/db/install.sql
+++ database/db/install.sql
@@ -42,3 +42,40 @@ create table public.image
 create unique index on image (post_id, url);
 
 comment on column public.image.file_size is 'File size in bytes';
+
+create view cameras_stat as
+select exif_camera_model,
+       sum(case when date_trunc('y', date_published) = '2008-01-01' then 1 else 0 end) as "2008",
+       sum(case when date_trunc('y', date_published) = '2009-01-01' then 1 else 0 end) as "2009",
+       sum(case when date_trunc('y', date_published) = '2010-01-01' then 1 else 0 end) as "2010",
+       sum(case when date_trunc('y', date_published) = '2011-01-01' then 1 else 0 end) as "2011",
+       sum(case when date_trunc('y', date_published) = '2012-01-01' then 1 else 0 end) as "2012",
+       sum(case when date_trunc('y', date_published) = '2013-01-01' then 1 else 0 end) as "2013",
+       sum(case when date_trunc('y', date_published) = '2014-01-01' then 1 else 0 end) as "2014",
+       sum(case when date_trunc('y', date_published) = '2015-01-01' then 1 else 0 end) as "2015",
+       sum(case when date_trunc('y', date_published) = '2016-01-01' then 1 else 0 end) as "2016",
+       sum(case when date_trunc('y', date_published) = '2017-01-01' then 1 else 0 end) as "2017",
+       count(*) as "total"
+  from image i
+  join post p on p.id = i .post_id
+  group by 1
+  having count(*) >= 100
+  order by 1;
+
+create view most_popular_dimensions as
+with data as (
+    select date_trunc('y', date_published) as date_published,
+           format('%s x %s', greatest(width, height), least(width, height)) as image_size,
+           count(*)
+      from image i
+      join post p on p.id = i.post_id
+      group by 1, 2 order by 1 asc, 3 desc
+),
+sorted_data as (
+    select t.*,
+           row_number() over (partition by date_published order by "count" desc)
+      from data t
+    order by 1, 4
+) select *
+    from sorted_data
+   where "row_number" < 3;
