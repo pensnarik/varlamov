3ca199f9f6f94cb70c2ade22454d54b2cdebfd6c
Andrey Zhidenkov <pensnarik@gmail.com>
2020-01-15 11:26:42 +0100

    Replace PostgreSQL container with its upstream version

--- database/db/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-psql -U postgres -f drop.sql -v database=database
-psql -U postgres -f install.sql -v database=database
--- database/db/drop.sql
+++
@@ -1,3 +0,0 @@
-UPDATE pg_database SET datallowconn = 'false' WHERE datname = :'database';
-SELECT pg_terminate_backend(pid) from pg_stat_activity where datname = :'database';
-DROP DATABASE IF EXISTS :database;
